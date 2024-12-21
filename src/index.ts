1a8c6b63c4a8333144c84e9a5c1c4ba8cd4ca466
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 <Poula-Karem>
2024-12-22 00:28:48 +0200

    removing mongoose.connection

--- src/index.ts
+++ src/index.ts
@@ -10,12 +10,12 @@ app.use(express.json());
 
 // mongoose.connect(`mongodb://localhost:27017/greenWorld`);
 
-mongoose.connect(`mongodb+srv://poulakarem:I9Mo2yABmg3zpgqO@green-world-db.4znn6.mongodb.net/`);
+mongoose.connect(`mongodb+srv://poulakarem:I9Mo2yABmg3zpgqO@green-world-db.4znn6.mongodb.net/database`);
 
 
 // mongoose.connect(`mongodb+srv://poulakarem:I9Mo2yABmg3zpgqO@green-world-db.4znn6.mongodb.net`);
 
-const db = mongoose.connection;
+// const db = mongoose.connection;
 
 // db.on("error", () => console.log("Connection Error!"));
 // db.once("open", () => console.log("Connected to mongoDB..."));
