187e38d0871f1f9e6d07342557c11e25bd0b70e0
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 <Poula-Karem>
2024-12-22 00:10:31 +0200

    mongo connection

--- src/index.ts
+++ src/index.ts
@@ -17,8 +17,8 @@ mongoose.connect(`mongodb+srv://poulakarem:I9Mo2yABmg3zpgqO@green-world-db.4znn6
 
 const db = mongoose.connection;
 
-db.on("error", () => console.log("Connection Error!"));
-db.once("open", () => console.log("Connected to mongoDB..."));
+// db.on("error", () => console.log("Connection Error!"));
+// db.once("open", () => console.log("Connected to mongoDB..."));
 
 app.use(vercelRoutes)
 app.use(userRoutes);
