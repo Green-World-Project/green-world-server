cd4bb8e07d6d7f84c07e522f0c056bf119534f75
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 <Poula-Karem>
2024-12-22 18:11:55 +0200

    green-world

--- src/index.ts
+++ src/index.ts
@@ -12,28 +12,6 @@ app.use(express.json());
 
 // mongoose.connect(`mongodb://localhost:27017/greenWorld`);
 
-// mongoose.connect(`mongodb+srv://poulakarem:I9Mo2yABmg3zpgqO@green-world-db.4znn6.mongodb.net/database`);
-
-// mongoose.connect(`mongodb+srv://poulakarem:I9Mo2yABmg3zpgqO@green-world-db.4znn6.mongodb.net`);
-
-// const db = mongoose.connection;
-
-// db.on("error", () => console.log("Connection Error!"));
-// db.once("open", () => console.log("Connected to mongoDB..."));
-
-
-
-
-// const uri = "mongodb+srv://poulakarem:I9Mo2yABmg3zpgqO@green-world-db.4znn6.mongodb.net/?retryWrites=true&w=majority&appName=green-world-db";
-
-
-// Track the connection status globally to reuse connections
-let isConnected: boolean = false;
-
-/**
- * Connect to MongoDB.
- */
-
 mongoose.connect(process.env.MONGODB_URI || '');
 
 app.use(vercelRoutes)
