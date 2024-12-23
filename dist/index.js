34953d9ec30cb36b772ba85ddcdd74def12fb8d2
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 <Poula-Karem>
2024-12-23 16:20:31 +0200

    tsc

--- dist/index.js
+++ dist/index.js
@@ -10,9 +10,12 @@ const app = (0, express_1.default)();
 const port = 3000;
 app.use(express_1.default.json());
 // mongoose.connect('mongodb+srv://vercel-admin-user-676834cadc6de46ef028bac1:kfzmCWFGbyb0tZRQ@cluster0.bdm0t.mongodb.net/greenWorldDatabase?retryWrites=true&w=majority');
-mongoose_1.default.connect(process.env.MONGODB_URI || '');
+mongoose_1.default.connect(process.env.MONGODB_URI || 'mongodb+srv://vercel-admin-user-676834cadc6de46ef028bac1:kfzmCWFGbyb0tZRQ@cluster0.bdm0t.mongodb.net/greenWorldDatabase?retryWrites=true&w=majority');
+const db = mongoose_1.default.connection;
+db.on("error", () => console.log("Connection Error!"));
+db.once("open", () => console.log("Connected to mongoDB..."));
 app.use(userRoutes_1.default);
 app.get('/vercel', (req, res) => {
-    res.json({ message: "Server is running on port" });
+    res.json({ message: "Server is running on vercel" });
 });
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
