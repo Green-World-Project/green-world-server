bc2a38a81c1de956c25dad9b87da2a5ebbbe7481
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 <Poula-Karem>
2024-12-23 15:40:51 +0200

    server is running route

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import express from 'express';
+import express, { Request, Response } from 'express';
 import mongoose from "mongoose";
 import userRoutes from "./routes/userRoutes";
 
@@ -7,8 +7,14 @@ const port = 3000;
 
 app.use(express.json());
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb+srv://vercel-admin-user-676834cadc6de46ef028bac1:kfzmCWFGbyb0tZRQ@cluster0.bdm0t.mongodb.net/greenWorldDatabase?retryWrites=true&w=majority');
+// mongoose.connect('mongodb+srv://vercel-admin-user-676834cadc6de46ef028bac1:kfzmCWFGbyb0tZRQ@cluster0.bdm0t.mongodb.net/greenWorldDatabase?retryWrites=true&w=majority');
+
+mongoose.connect(process.env.MONGODB_URI || '');
 
 app.use(userRoutes);
+
+app.get('/', (req: Request, res: Response) => {
+    res.send('Server is running on port')
+})
 
 app.listen(port, () => console.log(`Server is running on port ${port}...`));
